cbfac18c8e5f3dc938e848d5ae81208930e09710
Coady Duffney <coadyduffney@hotmail.com>
2018-10-04 11:27:46 -0300

    added tooltip ajax request

--- js/main.js
+++ js/main.js
@@ -12,3 +12,20 @@
 $('#toTop').on('click', function() {
   $('.container').animate({ scrollTop: 0 }, 1000);
 });
+
+$('#gear a').on('mouseover', function() {
+  const link = this.href;
+  item_id = link.toString().slice(27);
+  let url = `https://classicdb.ch/ajax.php?item=${item_id}&power`;
+
+  jQuery.ajax({
+    type: 'GET',
+    url: url,
+    success: function(res) {
+      console.log(res);
+    },
+    error: function(err) {
+      console.log(err);
+    }
+  });
+});
